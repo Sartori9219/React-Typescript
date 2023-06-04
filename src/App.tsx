b48d4d322ecbb52b97f9bf072718fe951c862a14
sartori0210 <Sartori93306@gmail.com>
2023-06-04 06:41:03 -0700

    first

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <blockquote cite="My name is Mirko">
+      What is your name?
+    </blockquote>
+  );
+}
+
+export default App;
